8cbd45cc1813e239b4a1954ddbd17ef03ca1d751
Gio <giovanni.lodi42@gmail.com>
2014-12-09 14:11:07 +1100

    Boilerplate project + liftoff

---
+++ podstension/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  podstension
+//
+//  Created by Gio on 09/12/2014.
+//  Copyright (c) 2014 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
